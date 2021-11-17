8ff37c3dbc79949df639b5fe14a0e90f0d2fb4e3
Stephen Barkan <stephen@doist.com>
2021-11-17 17:41:14 -0600

    lots o stuff

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -32,24 +32,30 @@ module.exports = {
       xl: "20px",
       full: "999px",
     },
+    transitionTimingFunction: {
+      DEFAULT: "cubic-bezier(.2,.8,.2,1)",
+    },
     boxShadow: {
+      "inset-stroke": "0px 0px 0px 0.6px inset rgba(0, 0, 0, 0.17)",
+      "applet-header":
+        "0px 1px 1px 0px hsla(var(--shadow-color) / 0.07), 0px 1px 1px 0px rgba(var(--clr-solid), 0.25), 0px 0px 0px 0.6px hsla(var(--shadow-color) / 0.3)",
       "btn-basic":
-        "0px 1px 1px rgba(0, 0, 0, 0.07), 0px 0px 0px 0.6px rgba(0, 0, 0, 0.17)",
-      "btn-inset-basic":
-        "0px 1px 1px inset rgba(0, 0, 0, 0.05), 0px 0px 0px 0.6px rgba(0, 0, 0, 0.17)",
+        "0px 1px 1px hsla(var(--shadow-color) / 0.40), 0px 0px 0px 0.6px hsla(var(--shadow-color) /  0.40)",
+      "btn-basic-inset":
+        "0px 1px 1px inset hsla(var(--shadow-color) / 0.2), 0px 0px 0px 0.6px hsla(var(--shadow-color) / 0.40)",
+      "list-inset": "0px 1px 1px inset hsla(var(--shadow-color) / .5)",
       "btn-highlight":
         "0px 0px 0px 0.6px #005CAF, 0px 1px 1px rgba(0, 0, 0, 0.07)",
-      topbar: "0px 0px 0px 0.6px rgba(0, 0, 0, 0.17)",
+      topbar:
+        "0px 1px 1px hsla(var(--shadow-color) / 0.40), 0px 1px 40px 0px rgba(var(--clr-solid), 0.15), 0px 0px 0px 0.6px hsla(var(--shadow-color) / 0.4)",
+      body: "0px 0px 40px 0px inset rgba(var(--clr-solid), 0.15)",
       focus: "0px 0px 0px 2px #5A98F6, 0px 0px 0px 4px #DCEAFF",
-      //   shadows generated https://www.joshwcomeau.com/shadow-palette/
-      // oomph: .03
-      // crispy: 0
-      //light position: centered like 1/3 up
-      // resolution .75
+      // shadows generated https://www.joshwcomeau.com/shadow-palette/
+      // oomph: .03, crispy: 0, light position: centered like 1/3 up, resolution .75
       "applet-idle":
-        "0px 0.4px 0.6px hsl(var(--shadow-color) / 0), 0px 1.5px 2.3px hsl(var(--shadow-color) / 0.09), 0px 3px 4.5px hsl(var(--shadow-color) / 0.19), 0px 5.9px 8.9px hsl(var(--shadow-color) / 0.28), 0 0 0 0.6px rgba(64, 64, 76, 0.1)",
+        "0px 0.4px 0.6px hsl(var(--shadow-color) / 0), 0px 1.5px 2.3px hsl(var(--shadow-color) / 0.09), 0px 3px 4.5px hsl(var(--shadow-color) / 0.19), 0px 5.9px 8.9px hsl(var(--shadow-color) / 0.28), 0 0 0 0.6px hsla(var(--shadow-color) / .5)",
       "applet-active":
-        "0px 0.4px 0.6px hsl(var(--shadow-color) / 0), 0px 2.3px 3.5px hsl(var(--shadow-color) / 0.04), 0px 4px 6px hsl(var(--shadow-color) / 0.09), 0px 5.7px 8.6px hsl(var(--shadow-color) / 0.13), 0px 7.9px 11.9px hsl(var(--shadow-color) / 0.17), 0px 10.9px 16.4px hsl(var(--shadow-color) / 0.22), 0px 15px 22.5px hsl(var(--shadow-color) / 0.26), -0.1px 20.6px 30.9px hsl(var(--shadow-color) / 0.3), 0 0 0 0.6px rgba(64, 64, 76, 0.1)",
+        "0px 0.4px 0.6px hsl(var(--shadow-color) / 0), 0px 2.3px 3.5px hsl(var(--shadow-color) / 0.04), 0px 4px 6px hsl(var(--shadow-color) / 0.09), 0px 5.7px 8.6px hsl(var(--shadow-color) / 0.13), 0px 7.9px 11.9px hsl(var(--shadow-color) / 0.17), 0px 10.9px 16.4px hsl(var(--shadow-color) / 0.22), 0px 15px 22.5px hsl(var(--shadow-color) / 0.26), -0.1px 20.6px 30.9px hsl(var(--shadow-color) / 0.3), 0 0 0 0.6px hsla(var(--shadow-color) / .5)",
     },
     backdropBlur: {
       DEFAULT: "20px",
@@ -79,7 +85,7 @@ module.exports = {
         "bg-hover": "var(--btn-bg-hover)",
       },
       clr: {
-        solid: "var(--clr-solid)",
+        solid: "rgba(var(--clr-solid), 1)",
         tint: "var(--clr-tint)",
         highlight: "var(--clr-highlight)",
       },
