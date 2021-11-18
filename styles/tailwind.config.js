4c74133a082e80a63c5d31e35244c4cfd67c6eca
Stephen Barkan <stephen@doist.com>
2021-11-18 09:09:31 -0600

    fix scrolling on photos

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -36,22 +36,23 @@ module.exports = {
       DEFAULT: "cubic-bezier(.2,.8,.2,1)",
     },
     boxShadow: {
-      "inset-stroke": "0px 0px 0px 0.6px inset rgba(0, 0, 0, 0.17)",
+      // shadows generated https://www.joshwcomeau.com/shadow-palette/
+      // oomph: .03, crispy: 0, light position: centered like 1/3 up, resolution .75
+      "inset-stroke":
+        "0px 0px 0px 0.6px inset hsla(var(--shadow-color) /  0.40)",
       "applet-header":
-        "0px 1px 1px 0px hsla(var(--shadow-color) / 0.07), 0px 1px 1px 0px rgba(var(--clr-solid), 0.25), 0px 0px 0px 0.6px hsla(var(--shadow-color) / 0.3)",
+        "0px 1px 1px 0px hsla(var(--shadow-color) / 0.04), 0px 1px 1px 0px rgba(var(--clr-solid), 0.20), 0px 0px 0px 0.6px hsla(var(--shadow-color) / 0.3)",
       "btn-basic":
         "0px 1px 1px hsla(var(--shadow-color) / 0.40), 0px 0px 0px 0.6px hsla(var(--shadow-color) /  0.40)",
       "btn-basic-inset":
         "0px 1px 1px inset hsla(var(--shadow-color) / 0.2), 0px 0px 0px 0.6px hsla(var(--shadow-color) / 0.40)",
-      "list-inset": "0px 1px 1px inset hsla(var(--shadow-color) / .5)",
+      "list-inset": "0px 1px 1px inset hsla(var(--shadow-color) / .3)",
       "btn-highlight":
         "0px 0px 0px 0.6px #005CAF, 0px 1px 1px rgba(0, 0, 0, 0.07)",
       topbar:
-        "0px 1px 1px hsla(var(--shadow-color) / 0.40), 0px 1px 40px 0px rgba(var(--clr-solid), 0.15), 0px 0px 0px 0.6px hsla(var(--shadow-color) / 0.4)",
-      body: "0px 0px 40px 0px inset rgba(var(--clr-solid), 0.15)",
+        "0px 1px 1px hsla(var(--shadow-color) / 0.40), 0px 1px 40px 0px rgba(var(--clr-solid), 0.10), 0px 0px 0px 0.6px hsla(var(--shadow-color) / 0.4)",
+      body: "0px 0px 40px 0px inset rgba(var(--clr-solid), 0.2)",
       focus: "0px 0px 0px 2px #5A98F6, 0px 0px 0px 4px #DCEAFF",
-      // shadows generated https://www.joshwcomeau.com/shadow-palette/
-      // oomph: .03, crispy: 0, light position: centered like 1/3 up, resolution .75
       "applet-idle":
         "0px 0.4px 0.6px hsl(var(--shadow-color) / 0), 0px 1.5px 2.3px hsl(var(--shadow-color) / 0.09), 0px 3px 4.5px hsl(var(--shadow-color) / 0.19), 0px 5.9px 8.9px hsl(var(--shadow-color) / 0.28), 0 0 0 0.6px hsla(var(--shadow-color) / .5)",
       "applet-active":
